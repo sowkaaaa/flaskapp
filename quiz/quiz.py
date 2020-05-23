7b18cbd8a1f85bd3f38ef1993fe9d3e3c65a7c45
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2020-05-23 13:04:14 +0200

    Aktualizacja

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -3,35 +3,60 @@
 
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for, flash
 
 app = Flask(__name__)
 
 # konfiguracja aplikacji
 app.config.update(dict(
-    SECRET_KEY='bradzosekretnawartosc',
+	SECRET_KEY='bardzosekretnysekret',
 ))
 
 # lista pytań
 DANE = [{
-    'pytanie': 'Stolica Hiszpani, to:',  # pytanie
-    'odpowiedzi': ['Madryt', 'Warszawa', 'Barcelona'],  # możliwe odpowiedzi
-    'odpok': 'Madryt'},  # poprawna odpowiedź
-    {
-    'pytanie': 'Objętość sześcianu o boku 6 cm, wynosi:',
-    'odpowiedzi': ['36', '216', '18'],
-    'odpok': '216'},
-    {
-    'pytanie': 'Symbol pierwiastka Helu, to:',
-    'odpowiedzi': ['Fe', 'H', 'He'],
-    'odpok': 'He'},
+  'pytanie': 'Stolica Hiszpani, to:', 
+  'odpowiedzi': ['Madryt', 'Warszawa', 'Barcelona'],  
+  'odpok': 'Madryt'}, 
+  {
+  'pytanie': 'Objętość sześcianu o boku 6 cm, wynosi:',
+  'odpowiedzi': ['36', '216', '18'],
+  'odpok': '216'},
+  {
+  'pytanie': 'Symbol pierwiastka Helu, to:',
+  'odpowiedzi': ['Fe', 'H', 'He'],
+  'odpok': 'He'},
+  {
+  'pytanie': 'Potoczna nazwa Kanczyla Srebrnogrzbietego, to:',
+  'odpowiedzi': ['Sarna', 'Świnia rzeczna', 'Myszojeleń'],
+  'odpok': 'Myszojeleń'},
+   {
+  'pytanie': 'Co to są liczby parzyste:',
+  'odpowiedzi': ['To te liczby, które mają parę.', 'Liczby podzielne przez dwa.', 'Ktoś bez pary.'],
+  'odpok': 'Liczby podzielne przez dwa.'},
 ]
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    
     # return 'Cześć, tu Python!'
     return render_template('index.html', pytania=DANE)
 
+@app.route('/odp', methods=['GET', 'POST'])
+def odp():
+
+    if request.method == 'POST':
+        punkty = 0
+        odpowiedzi = request.form
+
+        for pnr, odp in odpowiedzi.items():
+            if odp == DANE[int(pnr)]['odpok']:
+                punkty += 1
+
+        flash('Liczba poprawnych odpowiedzi, to: {0}'.format(punkty))
+        return redirect(url_for('odp'))
+
+    # return 'Cześć, tu Python!'
+    return render_template('odp.html', pytania=DANE)
 
 if __name__ == '__main__':
     app.run(debug=True)
